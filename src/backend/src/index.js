5ba739234b52f37984fd0e8af2868c9e5808ea9c
Dima Smirnov <email@dimasmirnov.ru>
2018-11-07 20:30:38 +0300

    added emit

--- src/backend/src/index.js
+++ src/backend/src/index.js
@@ -4,14 +4,20 @@ const http = require('http').Server(app);
 
 const io = require('socket.io')(http);
 
-
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendFile(path.resolve(__dirname, '../../frontend/index.html'));
 });
 
-
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
     console.log('a user connected');
+
+    socket.on('click', function(msg) {
+        console.log('message: ' + msg);
+    });
+
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
 });
 
 http.listen(3000, function() {
